c4a18f9e1cf9e17e1ca5dc15bf74be7a0ecf05a9
NataliaVolik <log.litana@gmail.com>
2020-06-14 13:27:13 +0600

    get data fake api input page

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,30 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {HttpClient} from "@angular/common/http";
+
+export interface Todo {
+  completed: boolean
+  title: string
+  id?: number
+}
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'angular-httpclient';
+
+  todos: Todo [] = []
+
+  constructor(public http: HttpClient) {
+
+  }
+  ngOnInit(){
+    this.http.get<Todo []>('https://jsonplaceholder.typicode.com/todos/?_limit=2')
+      .subscribe(todos => {
+        console.log('Response: ', todos);
+        this.todos = todos;
+      })
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,7 @@ import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import {MatButtonModule} from "@angular/material/button";
 import {MatInputModule} from "@angular/material/input";
+import {HttpClientModule} from "@angular/common/http";
 
 @NgModule({
   declarations: [
@@ -15,6 +16,7 @@ import {MatInputModule} from "@angular/material/input";
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
+    HttpClientModule,
     MatButtonModule,
     MatInputModule
   ],
