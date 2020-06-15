797973e062f1c2b9d0d5e0a1a364cf13a7e68f97
NataliaVolik <log.litana@gmail.com>
2020-06-15 13:11:03 +0600

    rxjs operators and loading

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import {Component, OnInit} from '@angular/core';
 import {HttpClient} from "@angular/common/http";
+import {delay} from "rxjs/operators";
 
 export interface Todo {
   completed: boolean
@@ -15,19 +16,17 @@ export interface Todo {
 export class AppComponent implements OnInit{
   title = 'angular-httpclient';
 
-  todos: Todo [] = []
+  todos: Todo [] = [];
 
-  todoTitle = ''
+  todoTitle = '';
+
+  loading = false;
 
   constructor(public http: HttpClient) {
 
   }
   ngOnInit(){
-    this.http.get<Todo []>('https://jsonplaceholder.typicode.com/todos/?_limit=2')
-      .subscribe(todos => {
-        console.log('Response: ', todos);
-        this.todos = todos;
-      })
+   this.fetchTodos();
   }
   addTodo(){
     if(!this.todoTitle.trim()){
@@ -39,9 +38,19 @@ export class AppComponent implements OnInit{
     }
     this.http.post<Todo>('https://jsonplaceholder.typicode.com/todos', newTodo)
       .subscribe(todo => {
-        console.log('todo', todo);
+        // console.log('todo', todo);
         this.todos.push(todo);
         this.todoTitle = '';
+      })
+  }
+  fetchTodos(){
+    this.loading = true;
+    this.http.get<Todo []>('https://jsonplaceholder.typicode.com/todos/?_limit=2')
+      .pipe(delay(1500))
+      .subscribe(todos => {
+        // console.log('Response: ', todos);
+        this.todos = todos;
+        this.loading = false;
       })
   }
 }
