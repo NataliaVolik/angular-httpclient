420fc6c0f819605f8734881c0a1d2d713be9b00c
NataliaVolik <log.litana@gmail.com>
2020-06-26 15:03:36 +0600

    transferring logic to a service

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,7 @@
-import {Component, OnInit} from '@angular/core';
-import {HttpClient} from "@angular/common/http";
-import {delay} from "rxjs/operators";
+import { Component, OnInit } from '@angular/core';
+import { Todo, TodosService } from "./todos.service";
 
-export interface Todo {
-  completed: boolean
-  title: string
-  id?: number
-}
+
 
 @Component({
   selector: 'app-root',
@@ -22,42 +17,38 @@ export class AppComponent implements OnInit{
 
   loading = false;
 
-  constructor(public http: HttpClient) {
+  constructor(private todosService: TodosService) {
 
   }
   ngOnInit(){
    this.fetchTodos();
   }
-  addTodo(){
-    if(!this.todoTitle.trim()){
+  addTodo() {
+    if (!this.todoTitle.trim()) {
       return
     }
-    const newTodo: Todo = {
+
+    this.todosService.addTodo({
       title: this.todoTitle,
       completed: false
-    }
-    this.http.post<Todo>('https://jsonplaceholder.typicode.com/todos', newTodo)
-      .subscribe(todo => {
-        // console.log('todo', todo);
-        this.todos.push(todo);
-        this.todoTitle = '';
-      })
+    }).subscribe(todo => {
+      this.todos.push(todo);
+      this.todoTitle = '';
+    })
   }
+
   fetchTodos(){
     this.loading = true;
-    this.http.get<Todo[]>('https://jsonplaceholder.typicode.com/todos/?_limit=2')
-      .pipe(delay(1500))
+    this.todosService.fetchTodos()
       .subscribe(todos => {
-        // console.log('Response: ', todos);
         this.todos = todos;
         this.loading = false;
       })
   }
-
   removeTodo(id: number) {
-    this.http.delete<void>(`https://jsonplaceholder.typicode.com/todos/${id}`)
-      .subscribe(() => {
-       this.todos = this.todos.filter(t => t.id !== id)
-      })
+    this.todosService.removeTodo(id)
+    .subscribe(() => {
+     this.todos = this.todos.filter(t => t.id !== id)
+    })
   }
 }
